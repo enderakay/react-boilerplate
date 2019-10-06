ebf41cc27d7d288cbc8ac7ee3fc16cee30cdf4b7
Ender Akay <enderakay@gmail.com>
2019-10-07 00:31:25 +0200

    making tests run

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/testSetup.js']
+    setupFilesAfterEnv: ['<rootDir>/testSetup.js'],
+    moduleNameMapper: {
+        "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+      }
 }
